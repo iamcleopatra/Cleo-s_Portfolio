d3094b6a70d41e409cf54124a264fc64dab682e8
cynthia <cmashabane023@student.wethinkcode.co.za>
2024-11-15 16:00:56 +0200

    added some imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{html,js,jsx,ts,tsx,css}", // Add your file types here
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBlue: '#1E3A8A', // Replace with the desired shade
+        lightBlue: '#3B82F6',
+        bgGray: '#F3F4F6',
+        accent: '#10B981', // Example accent color
+      },
+    },
   },
   plugins: [],
 }
